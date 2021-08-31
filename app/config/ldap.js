0ad36f15ed1c311445b529bd886810c59f2fb4db
Gamis777 <42852437+Gamis777@users.noreply.github.com>
2021-08-30 21:58:08 -0500

    update_config

--- app/config/ldap.js
+++ app/config/ldap.js
@@ -1,8 +1,9 @@
 module.exports = {
-    HOST: process.env.LDAP_HOST || "127.0.0.1",
+    HOST: process.env.LDAP_HOST || "191.98.176.205",
     bindDN: process.env.LDAP_BIND_DN || "",
     baseDN: process.env.LDAP_BASE_DN || "",
     baseUser: process.env.LDAP_BASE_USER || "",
     TIMEOUT: process.env.LDAP_TIMEOUT || 5000,
     CONNECTTIMEOUT: process.env.LDAP_TIMEOUT || 1000,
+    // PORT: process.env.LDAP_TIMEOUT ||  389
 };
